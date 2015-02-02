ef6af2b0a2f6986443dc2ccbd5cf801d6ebb6135
kvzhao <kevinzhaio@gmail.com>
2015-02-02 21:32:34 +0800

    Read mat file

--- GenDataList.py
+++ GenDataList.py
@@ -1,37 +1,41 @@
-#! /usr/bin/env python
-
-import os, sys
-import re
 import fnmatch
+import os, sys
+import os.path
+import re
 import numpy as np
 import scipy.io as sio
 
-# root_dir = sys.argv[1]
-# assign root director
-result_dir = 'Results/'
-result_dir = 'Results/CSK/'
-print result_dir
+includes = ['*.mat', '*.jpg'] # for files only
+excludes = ['center-error.mat', 'center-error.jpg'] # for dirs and files
 
-fd = open('data_list', 'w')
+errfile = 'center-error'
 
-# prevent note.txt file
-excludes = ['*.txt']
+# transform glob patterns to regular expressions
+includes = r'|'.join([fnmatch.translate(x) for x in includes])
 excludes = r'|'.join([fnmatch.translate(x) for x in excludes]) or r'$.'
 
-for root, dirs, files in os.walk(result_dir):
-    dirs = [os.path.join(root, d) for d in dirs]
+data_source_root = '/home/kv/workspace/Trackers/ALOV'
+
+for root, dirs, files in os.walk('/home/kv/workspace/Trackers'):
+    # exclude dirs
+    dirs[:] = [os.path.join(root, d) for d in dirs]
+    dirs[:] = [d for d in dirs if not re.match(excludes, d)]
+    # exclude/include files
     files = [os.path.join(root, f) for f in files]
     files = [f for f in files if not re.match(excludes, f)]
+    files = [f for f in files if re.match(includes, f)]
 
-    # avoid empty list and sort the list with file name
-    if files:
-        files.sort()
-    for fname in files:
+    # gurantee the order of files
+    files.sort()
+    # start parsing the file
+
+    for idx, fname in enumerate(files):
+        print idx
         images = fname.split('/')
-        if 'center-error.jpg' in images:
-            images.remove('center-error.jpg')
-        if 'center-error.mat' in images:
-            images.remove('center-error.mat')
-            mat = sio.loadmat(root + '/center-error.mat')
-            fd.write('/'.join(images) + '\n')
-
+        if errfile + '.mat' in images or errfile + '.jpg' in images:
+            continue
+        mat = sio.loadmat('/'.join(images[:-1]) + '/center-error.mat')
+        score = np.array(mat['error'][0,1:])
+        print data_source_root + '/'.join(images[7:])
+        # is this right?
+        print score[idx]
