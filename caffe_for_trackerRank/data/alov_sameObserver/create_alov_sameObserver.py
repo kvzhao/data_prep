207b3b6ebaa8ebd6d5565c80e0f1ea1fd839d9b0
kvzhaio <kevinzhaio@gmail.com>
2015-02-11 11:36:26 +0800

    convert mean image

--- caffe_for_trackerRank/data/alov_sameObserver/create_alov_sameObserver.py
+++ caffe_for_trackerRank/data/alov_sameObserver/create_alov_sameObserver.py
@@ -21,6 +21,7 @@ height_flag = '-resize_width=225'
 
 # program
 image_converter = root_path + 'build/tools/convert_imageset.bin'
+image_mean_computer = root_path + 'build/tools/compute_image_mean.bin'
 
 # delete the existing files
 #call(['rm', '-rf', db_save_path + db_name_prefix + '*'])
@@ -32,3 +33,4 @@ for root, dirs, files in os.walk(lists_path):
         datum_list_path = lists_path + fname 
         db_filename = db_save_path + db_name_prefix + tracker_type
         call([image_converter, height_flag, width_flag, shuffle_flag, project_path, datum_list_path, db_filename])
+        call([image_mean_computer, db_filename, db_filename +'/mean.binaryproto'])
